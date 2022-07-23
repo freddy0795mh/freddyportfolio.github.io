503de564a2748e52a07f807ff8d8fac9037ecd3e
freddy0795mh <freddymh07952019@gmail.com>
2022-07-22 20:52:28 -0500

    Actualizacion v1.2

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function mensajeenviado(){
         confirmButtonText: 'Ok'
       })
 }
+
+function enviarmail(){
+	/**
+	 * Método que envía un e-mail a través del cliente que tenga por defecto
+	 * @param  {String} insTextoComp Texto que aparecerá dentro del mensaje
+	 */
+	function enviarCorreo(insTextoComp) {
+    var name = document.getElementById('').value
+    var email = document.getElementById('').value
+    var asunto = document.getElementById('').value
+    var mensaje = document.getElementById('').value
+			var sLink = "mailto:" + escape(sEmail)
+			 + "?subject=" + escape("Te han compartirdo el siguente texto")
+			 + "&body=" + insTextoComp;
+			window.location.href = sLink;
+	}
+}
